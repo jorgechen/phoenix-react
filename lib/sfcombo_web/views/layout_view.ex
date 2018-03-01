76ad9d97aab8b9bcf527dd2dfb7778457fefbe03
George C <george.chen@arundo.com>
2018-02-28 18:56:17 -0600

    https://www.dailydrip.com/blog/how-to-use-phoenix-with-webpack-react-redux.html

--- lib/sfcombo_web/views/layout_view.ex
+++ lib/sfcombo_web/views/layout_view.ex
@@ -1,3 +1,21 @@
 defmodule SFComboWeb.LayoutView do
   use SFComboWeb, :view
+
+  def js_script_tag do
+    if Mix.env == :prod do
+      # In production we'll just reference the file
+      "<script src=\"/js/app.js\"></script>"
+    else
+      # In development mode we'll load it from our webpack dev server
+      "<script src=\"http://localhost:8080/js/app.js\"></script>"
+    end
+  end
+
+  def css_link_tag do
+    if Mix.env == :prod do
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/app.css\" />"
+    else
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://localhost:8080/css/app.css\" />"
+    end
+  end
 end
